34b9c6375331a31f6b0bb3fb8819b951a598c57f
trinhlq <trinhlq@gmail.com>
2020-03-25 20:43:47 +0700

    Add krusader and vlc setup

---
+++ setup/krusader.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y krusader
+sudo apt-get install -y kate
+sudo apt-get install kio-extras
---
+++ setup/vlc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo add-apt-repository ppa:videolan/master-daily
+sudo apt-get update
+sudo apt-get install vlc
+
+
+
